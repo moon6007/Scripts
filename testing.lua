d029b2abae06a9b4278d469769cc4c859f113ef9
mo_on <74736899+moon734@users.noreply.github.com>
2023-09-03 14:17:11 +0100

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,14 +1,25 @@
+
 local robloxapi = {}
 
-function  robloxapi.GetXcsrfToken(theheaders)
-	local auth_response = request({
-		Url = "https://auth.roblox.com/v1/logout",
-		Method = "POST",
-		Headers = theheaders
-	})
-	
-	return auth_response.Headers["x-csrf-token"]
-end)
+-- Define the GetXcsrfToken function
+function robloxapi.GetXcsrfToken(cookie)
+    -- Create headers table with the cookie
+    local headers = {
+        ["Cookie"] = cookie,
+    }
 
+    -- Send a request to obtain the CSRF token
+    local auth_response = request({
+        Url = "https://auth.roblox.com/v1/logout",
+        Method = "POST",
+        Headers = headers,
+    })
+
+  
+        local csrfToken = auth_response.Headers["x-csrf-token"]
+   
+      
+    end
+end
 
 return robloxapi
